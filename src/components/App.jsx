c395c90abee6c99074843775cd7dd16ab5c74df6
Midland707 <rus14job@ukr.net>
2023-02-14 21:48:40 +0200

    add files xxx.json + add markup Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { test } from 'components/Profile/Profile';
+
 export const App = () => {
   return (
     <div
@@ -7,11 +9,20 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework - goit-react-hw-01-components
-      
+      {test}
     </div>
   );
 };
+
+// const elem1 = <span>Hello</span>;
+// const elem2 = <span>World</span>;
+
+// const jsxElement = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,42 @@
+// 1 - Профіль соціальної мережі
+// Необхідно створити компонент <Profile>, за допомогою якого ми могли б відображати інформацію про користувача соціальної мережі.
+// Дані про користувача лежать у файлі user.json.
+
+// import PropTypes from 'prop-types'; // ES6 перевіряє відповідність типів даних
+import user from 'components/Profile/user.json';
+
+const Profile = ({ avatar, username, tag, location, stats }) => (
+  <div class="profile">
+    <div class="description">
+      <img src={avatar} alt="User avatar" class="avatar" />
+      <p class="name">{username}</p>
+      <p class="tag">@{tag}</p>
+      <p class="location">{location}</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+);
+
+export const test = (
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+);
