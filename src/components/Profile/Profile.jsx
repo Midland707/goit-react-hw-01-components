94f542c55434830b9f66a71fc4204d7d85980ddf
Midland707 <rus14job@ukr.net>
2023-02-16 00:53:06 +0200

    add Profile.propTypes

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@
 // Необхідно створити компонент <Profile>, за допомогою якого ми могли б відображати інформацію про користувача соціальної мережі.
 // Дані про користувача лежать у файлі user.json.
 
-// import PropTypes from 'prop-types'; // ES6 перевіряє відповідність типів даних
+import PropTypes from 'prop-types'; // ES6 перевіряє відповідність типів даних
 import user from './user.json';
 import './Profile.css';
 
@@ -41,3 +41,15 @@ export const profile = (
     stats={user.stats}
   />
 );
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
